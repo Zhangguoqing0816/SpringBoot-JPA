83af2e797b9e430f9644968e5ffb6425115df5f4
qiaozenglu <18829892581@163.com>
2019-08-02 16:14:25 +0800

    1

--- src/main/java/com/me/testjpa/jpa/controller/TestController.java
+++ src/main/java/com/me/testjpa/jpa/controller/TestController.java
@@ -2,11 +2,14 @@ package com.me.testjpa.jpa.controller;
 
 import ch.qos.logback.core.net.SyslogOutputStream;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TestController {
 
     public static void main(String[] args) {
         System.out.println(new Date());
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sf.format(new Date()));
     }
 }
